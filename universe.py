b7411d5bba76f132c995f5c763675947b66e420c
PFBDASH <purdy.j.justin@gmail.com>
2025-12-21 10:51:21 -0800

    Create universe.py

---
+++ universe.py
@@ -0,0 +1,26 @@
+# universe.py
+# Defines the tradable universe for the SSI engine
+
+CRYPTO_SYMBOLS = [
+    "BTCUSD",
+    "ETHUSD",
+    "SOLUSD",
+    "XRPUSD",
+    "ADAUSD"
+]
+
+FOREX_SYMBOLS = [
+    "EURUSD",
+    "GBPUSD",
+    "USDJPY",
+    "AUDUSD",
+    "USDCAD"
+]
+
+OPTIONS_UNDERLYINGS = [
+    "SPY",
+    "QQQ",
+    "IWM",
+    "NVDA",
+    "TSLA"
+]
